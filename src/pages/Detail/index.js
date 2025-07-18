e9ae7f4dd38a4f8ce695637f9cd9449f8f6e064c
FWSMaKNoon <cohoachunhiem@gmail.com>
2025-07-18 16:48:50 +0700

    Changed src/App

--- src/pages/Detail/index.js
+++
@@ -1,9 +0,0 @@
-import classNames from 'classnames/bind';
-import styles from './Detail.module.scss';
-const cx = classNames.bind(styles);
-
-function Detail() {
-    return <div className={cx('app_container')}></div>;
-}
-
-export default Detail;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,4 @@
 import Home from '~/pages/Home';
-import Detail from '~/pages/Detail';
 import Favorites from '~/pages/Favorites';
 import View from '~/pages/View';
 import { Route } from 'react-router-dom';
@@ -7,12 +6,10 @@ import { Route } from 'react-router-dom';
 //Public Routes
 const publicRoutes = [
     { path: '/', component: Home },
-    { path: '/detail/products/:id', component: Detail },
     { path: '/favorites', component: Favorites },
     { path: '/view', component: View },
 ];
 
 //Private Routes
 const privateRoutes = [];
-<Route path="/favorites" element={<Favorites />} />;
 export { publicRoutes, privateRoutes };
